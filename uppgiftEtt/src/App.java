abf1f2a18771b3f1d43a017490b17d392e0e89fe
Hezoz <sizarkhalaf9@gmail.com>
2026-03-26 13:54:15 +0100

    Skapade uppgift 1

---
+++ uppgiftEtt/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!!");
+        System.out.println("Ska äta idag lax med potatis");
+    }
+}
