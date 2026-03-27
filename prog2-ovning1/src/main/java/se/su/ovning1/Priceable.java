f1a401693505795ec64d7a083611e1e1ccc56914
mashfi13 <mashfi13@gmail.com>
2026-03-27 17:52:10 +0100

    La till public för getPrice() och getVAT() metoderna

--- prog2-ovning1/src/main/java/se/su/ovning1/Priceable.java
+++ prog2-ovning1/src/main/java/se/su/ovning1/Priceable.java
@@ -2,9 +2,9 @@ package se.su.ovning1;
 
 public interface Priceable {
 
-    double getPrice();
+    public double getPrice();
 
-    double getVAT();
+    public double getVAT();
 
     default double getPriceWithVAT() {
         return getPrice() * (1 + getVAT());
