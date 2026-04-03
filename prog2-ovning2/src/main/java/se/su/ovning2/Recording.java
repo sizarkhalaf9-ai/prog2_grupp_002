d5ff63ef4b561acac9889bd9ff9e3a45e6a6c7fd
mashfi13 <mashfi13@gmail.com>
2026-04-03 20:03:50 +0200

    Märkte att Recording.java inte stämde överens med den som Isak (läraren hade föreberett). Jag har fixat detta och lagt till kommentaren som handlade om : Object equals men kunde lista ut hur man skulle få det att fungera. Se kommentar i Recording.java klassen och vi har inte ändrat på något annat.

--- prog2-ovning2/src/main/java/se/su/ovning2/Recording.java
+++ prog2-ovning2/src/main/java/se/su/ovning2/Recording.java
@@ -1,42 +1,41 @@
 package se.su.ovning2;
 
-import java.util.Objects;
+import java.util.Collection;
 import java.util.Set;
 
 public class Recording {
-
-  private final String title;
-  private final String artist;
   private final int year;
-  private final String format;
-  private final Set<String> genres;
+  private final String artist;
+  private final String title;
+  private final String type;
+  private final Set<String> genre;
 
-  public Recording(String title, String artist, int year, String format, Set<String> genres) {
+  public Recording(String title, String artist, int year, String type, Set<String> genre) {
     this.title = title;
-    this.artist = artist;
     this.year = year;
-    this.format = format;
-    this.genres = genres;
-  }
-
-  public String getTitle() {
-    return title;
+    this.artist = artist;
+    this.type = type;
+    this.genre = genre;
   }
 
   public String getArtist() {
     return artist;
   }
 
+  public Collection<String> getGenre() {
+    return genre;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getType() {
+    return type;
+  }
+
   public int getYear() {
     return year;
-  }
-
-  public String getFormat() {
-    return format;
-  }
-
-  public Set<String> getGenres() {
-    return genres;
   }
 
   /*
@@ -48,10 +47,16 @@ public class Recording {
    * Objects.equals(title, that.title) &&
    * Objects.equals(artist, that.artist);
    * }
-   * 
-   * 
+   *
+   *
    * public int hashCode() {
    * return Objects.hash(title, artist, year);
    * }
    */
+
+
+  @Override
+  public String toString() {
+    return String.format("{ %s | %s | %s | %d | %s }", artist, title, genre, year, type);
+  }
 }
