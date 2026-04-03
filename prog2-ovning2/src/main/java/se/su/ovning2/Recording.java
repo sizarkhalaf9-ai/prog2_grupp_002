3a78edc87f1305cdd084476a4665da905aa40b77
mashfi13 <mashfi13@gmail.com>
2026-04-03 11:32:20 +0200

    Försökte lägga till Object equals men kunde lista ut hur man skulle få det att fungera. Se kommentar i Recording.java klassen och vi har inte ändrat på något annat.

--- prog2-ovning2/src/main/java/se/su/ovning2/Recording.java
+++ prog2-ovning2/src/main/java/se/su/ovning2/Recording.java
@@ -1,45 +1,57 @@
 package se.su.ovning2;
 
-import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 public class Recording {
-  private final int year;
-  private final String artist;
+
   private final String title;
-  private final String type;
-  private final Set<String> genre;
+  private final String artist;
+  private final int year;
+  private final String format;
+  private final Set<String> genres;
 
-  public Recording(String title, String artist, int year, String type, Set<String> genre) {
+  public Recording(String title, String artist, int year, String format, Set<String> genres) {
     this.title = title;
-    this.year = year;
     this.artist = artist;
-    this.type = type;
-    this.genre = genre;
-  }
-
-  public String getArtist() {
-    return artist;
-  }
-
-  public Collection<String> getGenre() {
-    return genre;
+    this.year = year;
+    this.format = format;
+    this.genres = genres;
   }
 
   public String getTitle() {
     return title;
   }
 
-  public String getType() {
-    return type;
+  public String getArtist() {
+    return artist;
   }
 
   public int getYear() {
     return year;
   }
 
-  @Override
-  public String toString() {
-    return String.format("{ %s | %s | %s | %d | %s }", artist, title, genre, year, type);
+  public String getFormat() {
+    return format;
   }
+
+  public Set<String> getGenres() {
+    return genres;
+  }
+
+  /*
+   * public boolean equals(Object o) {
+   * if (this == o) return true;
+   * if (!(o instanceof Recording)) return false;
+   * Recording that = (Recording) o;
+   * return year == that.year &&
+   * Objects.equals(title, that.title) &&
+   * Objects.equals(artist, that.artist);
+   * }
+   * 
+   * 
+   * public int hashCode() {
+   * return Objects.hash(title, artist, year);
+   * }
+   */
 }
